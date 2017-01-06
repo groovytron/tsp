10edf9ab71d799a0dfb00741a49bf2cd7ef9d1e5
Julien M'Poy <julien.mpoy@gmail.com>
2017-01-06 12:12:43 +0100

    Tried to separate GUI from logic

---
+++ gui.py
@@ -0,0 +1,66 @@
+import pygame
+from pygame.locals import KEYDOWN, QUIT, MOUSEBUTTONDOWN, K_RETURN, K_ESCAPE
+import sys
+from travel import City
+
+
+class Gui:
+    """Gui screen using pygame."""
+    def __init__(self, cities=None):
+        self.screen_x = 500
+        self.screen_y = 500
+        self.city_color = [10, 10, 200]  # blue
+        self.city_radius = 3
+        self.font_color = [255, 255, 255]  # white
+        pygame.init()
+        self.window = pygame.display.set_mode((self.screen_x, self.screen_y))
+        pygame.display.set_caption('Exemple')
+        self.screen = pygame.display.get_surface()
+        self.font = pygame.font.Font(None, 30)
+        self.collecting = True
+
+        if cities:
+            self.cities = cities
+            self.draw()
+            self.wait_for_user_input()
+        else:
+            self.cities = {}
+            self.place_cities()
+
+    def wait_for_user_input(self):
+        while self.collecting:
+            for event in pygame.event.get():
+                if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE)):
+                    sys.exit(0)
+                elif event.type == KEYDOWN and event.key == K_RETURN:
+                    self.collecting = False
+
+    def place_cities(self):
+        city_counter = 0
+        while self.collecting:
+            for event in pygame.event.get():
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                    sys.exit(0)
+                elif event.type == KEYDOWN and event.key == K_RETURN:
+                    self.collecting = False
+                elif event.type == MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    name = "v{}".format(city_counter)
+                    self.cities[name] = City(name, pos)
+                    city_counter = city_counter + 1
+                    self.draw()
+
+    def draw(self):
+        self.screen.fill(0)
+
+        for city in self.cities.values():
+            pygame.draw.circle(
+                self.screen, self.city_color, city.position, self.city_radius
+            )
+
+        text = self.font.render(
+            "Nombre: {}".format(len(self.cities)), True, self.font_color
+        )
+        textRect = text.get_rect()
+        self.screen.blit(text, textRect)
+        pygame.display.flip()
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from city import City, Link
+from travel import City
+from gui import Gui
 
 if __name__ == "__main__":
     # dictionnaire contenant les villes
     # clé: nom de la ville, valeur: objet City
     cities = {}
 
-    with open('data/pb005.txt', encoding='utf-8') as positions_file:
-        for line in positions_file:
-            # pour chaque ligne, on crée une ville avec les coordonnées
-            # associées
-            cityname, x, y = line.split()
-            cities[cityname] = City(cityname, (int(x), int(y)))
-        print(cities)
+    # file_name = 'data/pb005.txt'
+    file_name = ''
 
-    import gui_parser
-    cities2 = gui_parser.get_cities()
-    print(cities2)
+    if file_name:
+        with open(file_name, encoding='utf-8') as positions_file:
+            for line in positions_file:
+                # pour chaque ligne, on crée une ville avec les coordonnées
+                # associées
+                cityname, x, y = line.split()
+                cities[cityname] = City(cityname, (int(x), int(y)))
+            gui = Gui(cities)
+    else:
+        gui = Gui()
